92d82cd0cf996afab01c1e5c1097ec19ee1b18b4
payo84 <payos84@hotmail.com>
2021-10-11 13:51:21 +0200

    PrimeraVersión

---
+++ PrimeraTarea.java
@@ -0,0 +1,8 @@
+public class PrimeraTarea {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo");
+        System.out.println("Modulo: Entornos de Desarrollo");
+		System.out.println("Esto lo a�ado para ver si GIT recoge los cambios");
+        System.out.println("Hacemos la prueba");
+    }
+}
