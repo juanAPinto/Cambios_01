5d2797842775cd63f01a1a4739542800ce46d733
payo84 <payos84@hotmail.com>
2021-10-22 17:03:11 +0200

    CambioGustavo

--- PrimeraTarea.java
+++ PrimeraTarea.java
@@ -1,9 +1,11 @@
 public class PrimeraTarea {
     public static void main(String[] args) {
-        System.out.println("Hola, mundo");
-        System.out.println("Modulo: Entornos de Desarrollo");
-		System.out.println("Esto lo a�ado para ver si GIT recoge los cambios");
-        System.out.println("Hacemos la prueba");
-        System.out.println("Quiero que se vea este cambio en GitHub");
+        System.out.print("G");
+        System.out.print("U");
+		System.out.print("S");
+        System.out.print("T");
+        System.out.print("A");
+        System.out.print("V");
+        System.out.print("0");
     }
 }
